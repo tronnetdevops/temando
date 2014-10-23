7589b389dd5df0577d392b12355b650b5ffaeee9
pcm <pcm@tronnet.me>
2014-10-23 16:28:36 -0700

    multi-processing requests to speed things up

---
+++ fetch-script.php
@@ -0,0 +1,125 @@
+<?php
+	/**
+	 * @todo  Change HTTP_REFERER to be a specific access domain
+	 *        ie: http://ontrapalooa.com
+	 */
+	ini_set('default_socket_timeout', 600);
+
+	require_once('modules/temando.php');
+
+	$obj_tem = new TemandoWebServices;
+
+	$creds = json_decode( file_get_contents("data/creds.json"), true );
+
+	$username = $creds["username"];
+	$password = $creds["password"];
+	$endpoint = 'http://api.temando.com/schema/2009_06/server.wsdl';
+
+	/**
+	 * Parameters passed in from runner file.
+	 * @var [type]
+	 */
+	
+	$memcacheKey = $argv[1];
+	$orderQuantity = $argv[2];
+	$country = $argv[3];
+	$code = $argv[4];
+	$suburb = $argv[5];
+	$price = $argv[6];
+	$paid = $argv[7];
+	$shippingType = $argv[8];
+	
+	error_log("SEDNING REQUEST FOR " . $orderQuantity);
+	$request = array(
+		'anythings' => array(
+			'anything' => array (
+				0 => array(
+					'class' => 'General Goods', 
+					'subclass' => 'Household Goods',
+					'packaging' => 'Box',
+					'qualifierFreightGeneralFragile' => 'N',
+					'weight' => 18000,
+					'length' => 30,
+					'width' => 20,
+					'height' => 20,
+					'distanceMeasurementType' => 'Centimetres',
+					'weightMeasurementType' => 'Grams',
+					'quantity' => $orderQuantity
+				),
+			),
+		),
+
+		'anywhere' => array (
+			'itemNature' => $shippingType,
+			'itemMethod' => 'Door to Door',
+
+			'originCountry' => 'AU',
+			'originCode' => '3192',
+			'originSuburb' => 'Cheltenham', 
+			'originIs' => 'Business', 
+
+			'originBusDock' => 'N', 
+			'originBusUnattended' => 'N', 
+			'originBusForklift' => 'N', 
+			'originBusLoadingFacilities' => 'N', 
+			'originBusInside' => 'N', 
+			'originBusNotifyBefore' => 'N',
+			'originBusLimitedAccess' => 'N', 
+			'originBusHeavyLift' => 'N', 
+			'originBusContainerSwingLifter' => 'N', 
+			'originBusTailgateLifter' => 'N', 
+
+
+			'destinationCountry' => $country, //'AU',
+			'destinationCode' => $code, //'4000', 
+			'destinationSuburb' => $suburb, //'BRISBANE', 
+			'destinationIs' => 'Business', 
+
+
+			'destinationBusDock' => 'N', 
+			'destinationBusPostalBox' => 'N', 
+			'destinationBusUnattended' => 'N', 
+			'destinationBusForklift' => 'N', 
+			'destinationBusLoadingFacilities' => 'N', 
+			'destinationBusInside' => 'N', 
+			'destinationBusNotifyBefore' => 'N', 
+			'destinationBusLimitedAccess' => 'N', 
+			'destinationBusHeavyLift' => 'N', 
+			'destinationBusContainerSwingLifter' => 'N', 
+			'destinationBusTailgateLifter' => 'N'
+		), 
+
+
+
+		'anytime' => array(
+			'readyDate' => date("Y-m-d", strtotime("this friday")), 
+			'readyTime' => 'PM'
+		),
+
+		// 'clientId' => '64514', 
+		// 'promotionCode' => 'A0001', 
+		'general' => array(
+			'goodsValue' => $price,
+			'termsOfTrade' => "Delivered Duty " . $paid,
+			'goodsCurrency' => 'AUD'
+		)
+	);
+					
+	$response = $obj_tem->getQuotesByRequest($request,$username,$password,$endpoint);
+
+	if ($_GET["debug"]){
+		error_log("RESPONSE:");
+		error_log(var_export($response, true));
+	}
+
+	$quotes = array();
+
+	foreach($response["quote"] as $quote){
+		$quotes[ $quote["deliveryMethod"] ] = $quote["basePrice"];
+	}
+
+	$totals[$orderQuantity] = $quotes;
+
+	$memcache->set($memcacheKey."::".$orderQuantity, $totals); 
+
+	exit();
--- index.php
+++ index.php
@@ -3,22 +3,14 @@
 	 * @todo  Change HTTP_REFERER to be a specific access domain
 	 *        ie: http://ontrapalooa.com
 	 */
+	ini_set('default_socket_timeout', 600);
+
 	header("Access-Control-Allow-Origin: " . rtrim($_SERVER["HTTP_ORIGIN"], "/"));
 	header("Access-Control-Allow-Credentials: true");
 
 	
 	$typicalMax = 16; // = 400
 	$booksInBox = 25;
-
-	require_once('modules/temando.php');
-
-	$obj_tem = new TemandoWebServices;
-
-	$creds = json_decode( file_get_contents("data/creds.json"), true );
-
-	$username = $creds["username"];
-	$password = $creds["password"];
-	$endpoint = 'http://api.temando.com/schema/2009_06/server.wsdl';
 
 	$price = 24.70;
 
@@ -37,123 +29,30 @@
 	$memcache->addServer('localhost', 11211);
 
 	if ($_GET["_clear"]){
-		$memcache->delete($memcacheKey);	
+		$memcache->delete($memcacheKey."::".$quantity);	
 	}
 
-	$cached = $memcache->get($memcacheKey);
+	$cached = $memcache->get($memcacheKey."::".$quantity);
 
 	if ($cached){
 		echo json_encode(array(
-			"data" => $cached[$quantity],
+			"data" => $cached,
 			"status" => array(
 				"code" => 0,
 				"message" => "Success!"
 			)
 		));
-
-		exit(10);
 	} else {
-		$totals = array();
-
 		for ($i=0;$i<$typicalMax;$i++){
 			$orderQuantity = $i+1;
-			error_log("SEDNING REQUEST FOR " . $orderQuantity);
-			$request = array(
-				'anythings' => array(
-					'anything' => array (
-						0 => array(
-							'class' => 'General Goods', 
-							'subclass' => 'Household Goods',
-							'packaging' => 'Box',
-							'qualifierFreightGeneralFragile' => 'N',
-							'weight' => 18000,
-							'length' => 30,
-							'width' => 20,
-							'height' => 20,
-							'distanceMeasurementType' => 'Centimetres',
-							'weightMeasurementType' => 'Grams',
-							'quantity' => $orderQuantity
-						),
-					),
-				),
-
-				'anywhere' => array (
-					'itemNature' => $shippingType,
-					'itemMethod' => 'Door to Door',
-
-					'originCountry' => 'AU',
-					'originCode' => '3192',
-					'originSuburb' => 'Cheltenham', 
-					'originIs' => 'Business', 
-
-					'originBusDock' => 'N', 
-					'originBusUnattended' => 'N', 
-					'originBusForklift' => 'N', 
-					'originBusLoadingFacilities' => 'N', 
-					'originBusInside' => 'N', 
-					'originBusNotifyBefore' => 'N',
-					'originBusLimitedAccess' => 'N', 
-					'originBusHeavyLift' => 'N', 
-					'originBusContainerSwingLifter' => 'N', 
-					'originBusTailgateLifter' => 'N', 
-
-
-					'destinationCountry' => $country, //'AU',
-					'destinationCode' => $code, //'4000', 
-					'destinationSuburb' => $suburb, //'BRISBANE', 
-					'destinationIs' => 'Business', 
-
-
-					'destinationBusDock' => 'N', 
-					'destinationBusPostalBox' => 'N', 
-					'destinationBusUnattended' => 'N', 
-					'destinationBusForklift' => 'N', 
-					'destinationBusLoadingFacilities' => 'N', 
-					'destinationBusInside' => 'N', 
-					'destinationBusNotifyBefore' => 'N', 
-					'destinationBusLimitedAccess' => 'N', 
-					'destinationBusHeavyLift' => 'N', 
-					'destinationBusContainerSwingLifter' => 'N', 
-					'destinationBusTailgateLifter' => 'N'
-				), 
-
-
-
-				'anytime' => array(
-					'readyDate' => date("Y-m-d", strtotime("this friday")), 
-					'readyTime' => 'PM'
-				),
-
-				// 'clientId' => '64514', 
-				// 'promotionCode' => 'A0001', 
-				'general' => array(
-					'goodsValue' => $price,
-					'termsOfTrade' => "Delivered Duty " . $paid,
-					'goodsCurrency' => 'AUD'
-				)
-			);
-							
-			$response = $obj_tem->getQuotesByRequest($request,$username,$password,$endpoint);
-
-			if ($_GET["debug"]){
-				var_dump($response);
-				error_log("RESPONSE:");
-				error_log(var_export($response, true));
-			}
-
-			$quotes = array();
-
-			foreach($response["quote"] as $quote){
-				$quotes[ $quote["deliveryMethod"] ] = $quote["basePrice"];
-				// if ($quote["deliveryMethod"] == "GENERAL ROAD"){
-				// 		// echo "\$".$quote["basePrice"] ." - ". $quote["deliveryMethod"] ."<br/>";
-				// }
-			}
-
-			$totals[$orderQuantity] = $quotes;
+			Proc_Close (Proc_Open ("php -f ./fetch-script $memcacheKey $orderQuantity $country $code $suburb $price $paid $shippingType &> /dev/null &", Array (), $orderQuantity));
 		}
 
-		$memcache->set($memcacheKey, $totals); 
-		
-		exit();
+		echo json_encode(array(
+			"data" => array(),
+			"status" => array(
+				"code" => 1,
+				"message" => "Building shipping matrix!"
+			)
+		));
 	}
